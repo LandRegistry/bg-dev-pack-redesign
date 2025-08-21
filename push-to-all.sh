62439f33119de0bd22baf947dd2f676ff5f720ca
Luke.Gilfoyle <Luke.Gilfoyle@landregistry.gov.uk>
2025-08-21 11:37:30 +0100

    chore: make push script quieter

--- push-to-all.sh
+++ push-to-all.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Check if origin is correct
-if git remote -v show origin | grep github; then
+if git remote -v show origin | grep github &> /dev/null; then
     echo "You must clone the non-github version for this script to be useful."
     exit 0
 fi
 
 # Check if github origin is setup
-if git remote -v show github | grep github; then
-    echo "Github remote is already setup"
+if git remote -v show github | grep github &> /dev/null; then
+    echo "Github remote is setup"
 else
-    echo "Github remot needs to be setup"
+    echo "Github remote needs to be setup"
     git remote add -m main github git@github.com:LandRegistry/bg-dev-pack-redesign.git
 fi
 
